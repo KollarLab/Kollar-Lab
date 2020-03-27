I think I finally fixed the bugs with switching in and out of averaging mode for the Acqiris. In the end the fix was to have separate flags for averaging at the python and hardware levels. First, python knows which mode its in, and adjusts the allowed number of samples accordingly. Then it sets the samples to the hardware using the C configure acquisition function. This makes sure that the number of samples is low before trying to turn on averaging. Only then can the driver-level avergaing flags be turned on and the number of averages turned up. Or something like that. I'm still not entirely sure why I don't have problems switching back out of avergaing mode.

Control/Acqiris_development/Acqiris_testScript_forAcqiris.py
@@ -41,9 +41,11 @@ sys.path.append(IVIbinPath)
 #minimum acquisition
 ###############
 
-#card = Acqiris(hardwareAddress)
+card = Acqiris(hardwareAddress)
 
-card.samples = 1024*600 #too long for averaging mode, but fine for regular
+
+
+card.samples = 1024*515 + 2 #too long for averaging mode, but fine for regular
 card.segments = 2
 card.averages = 1
 card.triggerDelay = 25*10**-6
@@ -53,10 +55,10 @@ data1, data2 = card.ReadAllData() #read data for the active channels.
 ts = 10**6* scipy.arange(0, data1.shape[1],1.)*1/card.sampleRate
 #
 print('Took regular data')
-#
-#card.ReInitialize()
 
-card.samples = 1024*500 #going down to something short enogh for regular
+card.ReInitialize()
+
+card.samples = 1024*513 + 2 #going down to something short enogh for regular
 card.segments = 2
 card.averages = 100
 card.triggerDelay = 25*10**-6
@@ -67,15 +69,17 @@ avTs = 10**6* scipy.arange(0, avData1.shape[1],1.)*1/card.sampleRate
 
 print('Took regular averaged data')
 
-
-#card.samples = 1024*513 #too long for averaging mode, but fine for regular
+#card.samples = 1024*513 + 2 #too long for averaging mode, but fine for regular
 #card.segments = 2
 #card.averages = 1
+#card.triggerDelay = 25*10**-6
 #card.SetParams() #pushes default to to card if the fields haven't been edited
 #card.ArmAndWait() #initiates aquisition and calibrates if need be
 #data1, data2 = card.ReadAllData() #read data for the active channels.
+#ts = 10**6* scipy.arange(0, data1.shape[1],1.)*1/card.sampleRate
 ##
 #print('Took regular data again')
+
 
 pylab.figure(4)
 pylab.clf()
@@ -91,7 +95,7 @@ ax.legend(loc = 'upper left')
 ax = pylab.subplot(2,2,2)
 pylab.plot(ts,data2[0,:], label = 'single')
 pylab.plot(avTs, avData2[0,:], label = 'averaged')
-#ax.set_ylim([-0.04,0.04])
+ax.set_ylim([-0.04,0.04])
 pylab.title('Channel 2, Segment 1')
 pylab.ylabel('Voltage')
 pylab.xlabel('t ($\mu$s)')
@@ -110,7 +114,7 @@ ax.legend(loc = 'upper left')
 ax = pylab.subplot(2,2,4)
 pylab.plot(ts, data2[1,:], label = 'single')
 pylab.plot(avTs, avData2[1,:], label = 'averaged')
-#ax.set_ylim([-0.04,0.04])
+ax.set_ylim([-0.04,0.04])
 pylab.title('Channel 2, Segment 2')
 pylab.ylabel('Voltage')
 pylab.xlabel('t ($\mu$s)')
@@ -124,56 +128,7 @@ pylab.show()
 
 
 
-#card.samples = 1024*500 #going down to something short enogh for regular
-#card.segments = 100
-#card.averages = 1
-#card.triggerDelay = 25*10**-6
-#card.SetParams() #pushes default to to card if the fields haven't been edited
-#card.ArmAndWait() #initiates aquisition and calibrates if need be
-#data1, data2 = card.ReadAllData() #read data for the active channels.
-#ts2 = 10**6* scipy.arange(0, data1.shape[1],1.)*1/card.sampleRate
-#
-#card.samples = 1024*500 #going down to something short enogh for regular
-#card.segments = 1
-#card.averages = 100
-#card.triggerDelay = 25*10**-6
-#card.SetParams() #pushes default to to card if the fields haven't been edited
-#card.ArmAndWait() #initiates aquisition and calibrates if need be
-#avData1, avData2 = card.ReadAllData() #read data for the active channels.
-#
-#
-#
-#pylab.figure(5)
-#pylab.clf()
-#ax = pylab.subplot(2,2,1)
-#pylab.title('Ch1: 100 single segments')
-#for seg in range(0, card.segments):
-#    pylab.plot(ts2, data1[seg,:])
-#
-#pylab.subplot(2,2,3)
-#pylab.plot(ts2, avData1)
-#pylab.title('Ch2: average of 100 segments')    
-#
-#
-#
-#ax = pylab.subplot(2,2,2)
-#pylab.title('Ch2: 100 single segments')
-#for seg in range(0, card.segments):
-#    pylab.plot(ts2, data2[seg,:])
-#    
-#pylab.subplot(2,2,4)
-#pylab.plot(ts2, avData2)
-#pylab.title('Ch2: average of 100 segments')    
-#
-#pylab.show()
-
-
-
-
-
-#card.close() #terminate connection with the card. which we probably don't want to do.
-
-
+#card.close()
 
 
 