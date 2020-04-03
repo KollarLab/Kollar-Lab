a346756573ba700c9fba576b87f6089a63864610
Kollar_Control2 <akollar@umd.edu>
2020-04-03 10:35:51 -0400

    trying more tests from Acqiris. A bunch of test files, copied dlls, and junk is now in the 'stupid test' folder

--- Control/Acqiris_development/AcqirisPythonBeta_testScript.py
+++ Control/Acqiris_development/AcqirisPythonBeta_testScript.py
@@ -27,6 +27,7 @@ import sys
 
 #hardwareAddress = "PXI23::0::0::INSTR"
 #
+dotNetPath = "C:\\Program Files\\IVI Foundation\\IVI\\Microsoft.NET\\Framework64\\v4.0.30319\\Acqiris.AqMD3 3.6.7913\\"
 IVIbinPath = "C:\\Program Files\\IVI Foundation\\IVI\\Bin\\"
 AcqirisBinPath = r'C:\\Program Files\\Acqiris\\MD3\\bin\\'
 #IVIbinPath = "C:\\Program Files\\IVI Foundation\\IVI\\Bin"
@@ -38,6 +39,8 @@ if not IVIbinPath in sys.path:
     sys.path.append(IVIbinPath)
 if not AcqirisBinPath in sys.path:
     sys.path.append(AcqirisBinPath)
+if not dotNetPath in sys.path:
+    sys.path.append(dotNetPath)
 
 ###############
 #modified from:
