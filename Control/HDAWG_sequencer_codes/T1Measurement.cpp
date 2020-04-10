6addc28e48c3ea645ead479e7a72426f18fb1147
Martin Ritter <maritter@umd.edu>
2020-04-10 12:06:55 -0400

    Restructured sequencer code to take in times instead of samples and perform conversion internally. Also added for loop to perform some averaging

--- Control/HDAWG_sequencer_codes/T1Measurement.cpp
+++ Control/HDAWG_sequencer_codes/T1Measurement.cpp
@@ -3,24 +3,33 @@
 //_samplesparam_ should be used to specify the number of points in the waveforms
 //Wait time can be set either here or by formatting the string
 
-const NumSamples = 800;
-const waitInc    = 1000;
-const clearTime  = 1000;
-const markerPos  = 300;
+const NumSamples = _NumSamples_;
+const waitInc    = _waitInc_;
+const markerPos  = _markerPos_;
+const averages   = _averages_;
+const numMeas    = _numMeas_;
+const waitInit   = _waitInit_;
 
-wave w1   = gauss(NumSamples,NumSamples/2,NumSamples/8);
-wave w1f  = zeros(NumSamples); //Gaussian starting at 0
-wave mark = marker(NumSamples,1); //Make rectangular window around gaussian pulse to be used as trigger for digitizer or something
+wave w1       = gauss(NumSamples,NumSamples/2,NumSamples/8);
+wave w1_final = zeros(NumSamples); //Gaussian starting at 0
+wave w1_mark  = marker(NumSamples,1); //Make rectangular window around gaussian pulse to be used as trigger for digitizer or something
+
 cvar i;
 for(i=0;i<NumSamples;i++){
-  w1f[i]=w1[i]-w1[0]; //Remove offset from discrete gaussian
+  w1_final[i] = w1[i]-w1[0]; //Remove offset from discrete gaussian
 }
-wave w1ff=w1f+mark;
-for (i=0;i<5;i++){
-  playWave(w1ff);
-  waitWave();
-  wait(waitInc*i);
-  playWave(w1f);
-  waitWave();
-  wait(clearTime);
+
+wave w1_markers = w1_final + w1_mark;
+
+for (i=0 ; i<numMeas ; i++){
+  cvar j;
+  for (j=0 ; j<averages; j++){
+    //Wait for trigger on channel 1
+    waitDigTrigger(1)
+    playWave(w1_markers);
+    waitWave();
+    wait(waitInit+waitInc*i);
+    playWave(w1_final);
+    waitWave();
+  }
 }
