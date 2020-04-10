28b546aa6ca2b9bcfa0c4e11980f9518c6643a29
Martin Ritter <maritter@umd.edu>
2020-04-10 12:54:09 -0400

    First draft of exp script using new HDAWG functionality, stills needs testing. Will probably use settings file to hold all the exp settings as well as the HDAWG settings

--- Control/ExpScript.py
+++ Control/ExpScript.py
@@ -6,34 +6,36 @@ import time
 #RFsource=RFgen('TCPIP0::rssgs100a110739::inst0::INSTR') #SGS visa resource name
 
 ## Connect to HDAWG and initialize it 
-AWG = HDAWG('dev8163') #HDAWG device name
-AWG.enable_channels([0,1]) #turn on output for channels 0 and 1
-AWG.set_AWGamp([1.,1.],[0,1]) #set output amplitude
-AWG.enable_markers([0,1]) #enable markers for our channels 
+hdawg = HDAWG('dev8163') #HDAWG device name
+hdawg.AWGs[0].samplerate = '2.4GHz'
+hdawg.channelgrouping = '1x4'
+hdawg.Channels[0].configureChannel(amp=1.0,marker_out='Marker')
+hdawg.AWGs[0].Triggers[0].configureTrigger(slope='rising',channel='Trigger in 1')
 
 ## Read in the sequencer program we want to use
 progFile = open("HDAWG_sequencer_codes/T1Measurement.cpp",'r')
 rawprog  = progFile.read()
 loadprog = rawprog
+progFile.close
 
 ## Configure program to our parameters
-NumSamples = 800 #sets number of samples to use for the waveform (try to make it a multiple of 16 otherwise AWG pads with zeros)
-waitInc    = 1000 #sets increment of wait time between two pulses (in units of clock cycles, approx 3.3 ns)
-clearTime  = 1000 #time between two measurements (clock cycles)
-markerPos  = 300 #sets position of trigger relative to signal (0 is coicident)
+piAmp           = 1.0 #pi pulse amplitude, relative to output amplitude
+piTime          = 0.5e-6 #pi pulse length (s)
+piWidth         = piTime/8 #pi pulse width (gaussian) (s)
+qubit_lifetime  = 10e-6 #qubit lifetime (s)
+wait_increment  = 1e-6 #timestep between different iterations (s)
+marker_position = 0 #timing of marker signal (s)
+averages        = 10 #number of averages per setting
+number_configs  = 5 #number of different configurations
 
-loadprog = loadprog.replace('_NumSamples_', str(NumSamples))
-loadprog = loadprog.replace('_waitInc_', str(waitInc))
-loadprog = loadprog.replace('_clearTime_', str(clearTime))
-loadprog = loadprog.replace('_markerPos_', str(markerPos))
-AWG.load_program(loadprog)
+loadprog = loadprog.replace('_piAmp_', str(piAmp))
+loadprog = loadprog.replace('_piTime_', str(piTime))
+loadprog = loadprog.replace('_piWidth_', str(piWidth))
+loadprog = loadprog.replace('_qlifetime_', str(qubit_lifetime))
+loadprog = loadprog.replace('_waitInc_', str(wait_increment))
+loadprog = loadprog.replace('_markerPos_', str(marker_position))
+loadprog = loadprog.replace('_averages_', str(averages))
+loadprog = loadprog.replace('_numConfig_', str(number_configs))
+hdawg.load_program(loadprog)
 
-## Create waveforms we want to use for channels 1 and 2
-time    = np.linspace(-np.pi,np.pi, NumSamples)
-Isignal = np.sin(time)
-Qsignal = np.cos(time)
-
-#AWG.load_waveform(0,Isignal,Qsignal)
-
-## Run AWG program
-AWG.AWG_run()
+hdawg.AWGs[0].run()
