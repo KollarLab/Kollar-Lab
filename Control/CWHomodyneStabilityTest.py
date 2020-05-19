c5c2721ddb1669f61a467705179aa007d22c60ab
Martin Ritter <maritter@umd.edu>
2020-05-19 16:19:38 -0400

    Cleaned up code to make it more modular and removed unused imports/ code segments

--- Control/CWHomodyneStabilityTest.py
+++ Control/CWHomodyneStabilityTest.py
@@ -7,32 +7,46 @@ Created on Fri May  1 11:21:50 2020
 
 from Instruments.HDAWG import HDAWG
 from Instruments.SGS import RFgen
+from Acqiris_development.Acqiris import Acqiris
+#from userfuncs import freeze #don't think we need to import this, only needed in class declarations
+import userfuncs as uf
+
 import numpy
 import time
-import sys
-import scipy
+#import sys
 import pylab
-import scipy.optimize
+
 from mplcursors import cursor as datacursor
 from datetime import datetime
 
-
-from userfuncs import freeze
-import userfuncs as uf
-
-
-from Acqiris_development.Acqiris import Acqiris
-
-
+# Shouldn't need this since it should run in the Acqiris code where creating a card
+'''
 IVIbinPath = "C:\\Program Files\\IVI Foundation\\IVI\\Bin\\"
 if not IVIbinPath in sys.path:
     sys.path.append(IVIbinPath)
-    
+'''    
         
 def calibrate_mixer_IQ(freq, power, numPoints, measDur = 5e-6, verbose = False, showFig = True):
+    '''
+    Helper function to create IQ imbalance ellipse. Returns the axes, center and angle of the ellipse
+    Can draw the ellipse if desired
+    Arguments:
+        freq (int): frequency used for LO/RF generator
+        power (double): power (in dB) for RF generator
+        numPoints (int): number of points to take on the ellipse
+        measDur (double): time for digitizer to acquire dataset (default 5e-6)
+        verbose (bool): sets verbosity of output (default False)
+        showFig (bool): sets whether we draw a figure or not (default True)
+    Returns:
+        axes ([double]): array with the x and y axes of the ellipse
+        center ([double]): array with the coordinates of the ellipse
+        phi (double): angle of the ellipse (45 degrees means a circle in this case)
+    '''
+    
     freq_GHz = freq/1e9
     phases = numpy.linspace(0,360,numPoints)
-    
+
+    ## Digitizer card settings 
     card.triggerSlope = 'Rising'
     card.triggerLevel = 0.1
     card.averages = 1 #on-board averages
@@ -46,6 +60,7 @@ def calibrate_mixer_IQ(freq, power, numPoints, measDur = 5e-6, verbose = False, 
     card.samples = numpy.ceil(measDur*card.sampleRate)
     card.SetParams() #warning. this may round the number of smaples to multiple of 1024
     
+    ## SGS unit settings
     logen.set_Freq(freq_GHz)
     logen.set_Amp(12)
     logen.mod_Off()
@@ -56,7 +71,8 @@ def calibrate_mixer_IQ(freq, power, numPoints, measDur = 5e-6, verbose = False, 
     rfgen.mod_Off()
     rfgen.power_On()
 
-    time.sleep(0.05)
+    ## Wait for settings to percolate
+    time.sleep(0.5)
     
     Idata = numpy.zeros(card.samples)
     Qdata = numpy.zeros(card.samples)
@@ -122,46 +138,49 @@ def calibrate_mixer_IQ(freq, power, numPoints, measDur = 5e-6, verbose = False, 
     return axes, center, phi
 
 
-def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, coupling='LO', measure_time=900, savepath=r'C:\Users\Kollarlab\Desktop'):
-        
-    #################################
-    #Configuration settings
-    #################################
-    reference_signal   = ref
-    reference_freq_MHz = ref_freq
-    SGS_ref_freq       = SGS_ref_freq
-    coupling_type      = coupling
-    #Timing
-    short = numpy.ones(measure_time)
+def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, coupling='Ref', measure_time=900, savepath=r'C:\Users\Kollarlab\Desktop'):
+    '''
+    General useage function that performs a homodyne phase stability measurement. Produces a figure showing mixer
+    ellipticity and phase over time
+    Arguments:
+        ref (str) : Sets the reference used by the rf generator ('HDAWG' or 'None')
+        ref_freq (int) : Sets the external reference frequency (10,100,1000 MHz)
+        SGS_ref_freq (int) : Sets the reference frequency between the two SGS units (1000 MHz gives cleanest results)
+        coupling (str) : Sets coupling type between SGS units ('LO': direct coupling or 'Ref' uses reference signal to lock PLL)
+        measure_time (int) : Number of measurements (1 measurement~1s)
+        savepath (str) : Path where figure will be saved
+    '''
+    ## General reference configuration
+    reference_signal   = ref 
+    reference_freq_MHz = ref_freq 
+    SGS_ref_freq       = SGS_ref_freq 
+    coupling_type      = coupling 
     
-    ############
-    #measurement params
-    ###########
+    ## Measurement parameters
     measDur = 1e-6
     freq = 8e9
     power = 0             
-    
-    hdawg.Channels[2].analog_outs = [0.5,0]
-    hdawg.Channels[3].analog_outs = [0,1]
-    hdawg.Channels[2].configureChannel(amp=1.0)
+    freq_GHz = freq/1e9
+
+    ## HDAWG Settings
+    hdawg.Channels[2].analog_outs = [0.5,0] #set output amplitudes of sine generator
+    hdawg.Channels[3].analog_outs = [0,1] 
+    hdawg.Channels[2].configureChannel(amp=1.0) #set output range of sine generator
     hdawg.Channels[3].configureChannel(amp=2.0)
-    hdawg.OSCs[1].configure_sine(0,10e6)
+    hdawg.OSCs[1].configure_sine(0,10e6) #set frequency of sine generator
     hdawg.OSCs[1].configure_sine(1,reference_freq_MHz*1e6)
     
     logen.power_Off()
     rfgen.power_Off()
-    #rfgen.set_Internal_Reference() #RF gen by itself, to it's own drum
-    #logen.set_Internal_Reference() #LO gen following RF gen
-    time.sleep(0.5)
+    time.sleep(0.5) #Making sure that the settings have been applied
     
-    #calibrate the mixer for reference
-    mixerAxes, mixerCenter, mixerPhi = calibrate_mixer_IQ(freq, power, 35, measDur = 5e-6, verbose = False)
+    ## Calibrate the mixer for reference
+    mixerAxes, mixerCenter, mixerPhi = calibrate_mixer_IQ(freq, power, 35, measDur = 5e-6, verbose = False, showFig=False)
     xx, yy = uf.make_elipse(mixerAxes,  mixerCenter, mixerPhi, 150)
+    xmax = 1.1 * max(abs(xx))
+    ymax = 1.1 * max(abs(yy))
     
-    #########
-    #now look at homodyne versus time.
-    freq_GHz = freq/1e9
-    
+    ## Digitizer card settings 
     card.triggerSlope = 'Rising'
     card.triggerLevel = 0.1
     card.averages = 1 #on-board averages
@@ -175,10 +194,7 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
     card.samples = numpy.ceil(measDur*card.sampleRate)
     card.SetParams() #warning. this may round the number of smaples to multiple of 1024
     
-    
-    ##################################
-    #SGS settings
-    ##################################
+    ## SGS settings
     logen.set_Freq(freq_GHz)
     logen.set_Amp(12)
     logen.mod_Off()
@@ -186,6 +202,7 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
     rfgen.set_Freq(freq_GHz)
     rfgen.set_Amp(power)
     rfgen.mod_Off()
+    # Apply reference/ coupling that the user has set up
     if reference_signal == 'HDAWG':
         rfgen.set_External_Reference(freq = reference_freq_MHz)
     else:
@@ -203,29 +220,40 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
         logen.set_External_LO()
     logen.power_On() 
     
+    # Wait and make sure that the settings have been applied
+    #TO DO: write SGS function to check PLL status to avoid these long waits
     time.sleep(30)
+
+    #################################################
+    # Measurement and analysis
+    #################################################
     
-    timeSpacings = short
-    actualTimes = numpy.zeros(len(timeSpacings))
+    short = numpy.ones(measure_time) #multiply by an int to get larger time spacing (1 second at the moment)
+    timeSpacings = short #if desired, can concatenate multiple different time ranges/ spacings for especially large sets
+    actualTimes = numpy.zeros(len(timeSpacings)) #array to hold the 'real' wall time ellapsed in measurement
     
-    Is = numpy.zeros(len(timeSpacings))
-    Qs = numpy.zeros(len(timeSpacings))
-    Amps = numpy.zeros(len(timeSpacings))
-    Angles = numpy.zeros(len(timeSpacings))
-    
+    Is = numpy.zeros(len(timeSpacings)) #array to hold I data
+    Qs = numpy.zeros(len(timeSpacings)) #array to hold Q data
+    Amps = numpy.zeros(len(timeSpacings)) #array to hold amplitude data
+    Angles = numpy.zeros(len(timeSpacings)) #array to hold phase data
     
     plotSpacing = 30
-    fig1 = pylab.figure(1, figsize=(14, 8))
+    figure_size = (14,8)
+    fig1 = pylab.figure(1, figsize=figure_size)
     pylab.clf()
+
+    date  = datetime.now()
+    stamp = date.strftime('%Y%m%d_%H%M%S')
+    filename = 'CWHomodyne{}{}{}{}_{}'.format(reference_signal, reference_freq_MHz, coupling_type,SGS_ref_freq, stamp)
+    fullpath = savepath+'\\'+filename
+    figtitle = 'Homodyne Stability v. Time\n Reference Signal: {}, Ref frequency: {}, Coupling:{}, SGS ref freq:{}\n Path:{}'.format(reference_signal,reference_freq_MHz,coupling_type, SGS_ref_freq, fullpath)
     
     t0 = time.time()
     for tind in range(0, len(timeSpacings)):
         spacing = timeSpacings[tind]
-    #    print("current spacing = ", numpy.round(spacing,3))
         time.sleep(spacing)
         
-        
-        
+        #Read data from digitizer
         card.ArmAndWait()
         currT = time.time()
         print("current time = ", numpy.round(currT-t0,3))
@@ -245,23 +273,19 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
         
         actualTimes[tind] = currT - t0
     
-    
-        if numpy.mod(tind, plotSpacing) == 0:
-            fig1 = pylab.figure(1)
+        # Every plotspacing iterations, we want to update the figure and on the last iteration
+        if numpy.mod(tind, plotSpacing) == 0 or tind==len(timeSpacings-1):
+            #fig1 = pylab.figure(1)
             pylab.clf()
             ax = pylab.subplot(1,2,1)
-    #        pylab.plot(Is[0:tind], Qs[0:tind], linestyle = '', marker = 'o', markersize = 5, color = 'mediumblue')
             
-    #        cVec = actualTimes[0:tind]/sum(timeSpacings)
-    #        cmap = 'cividis'
-    #        pylab.scatter(Is[0:tind], Qs[0:tind], c = cVec, cmap = cmap , marker = 'o', s = 75, edgecolors = 'midnightblue', zorder = 2)
-            
+            #Plot I/Q data on parameteric plot to show IQ vector
             cVec = actualTimes[0:tind]
             cmap = 'cividis'
             pylab.scatter(Is[0:tind], Qs[0:tind], c = cVec, cmap = cmap , marker = 'o', s = 75,
                           vmin = 0, vmax = sum(timeSpacings), edgecolors = 'midnightblue', zorder = 2)
             
-            
+            #Plot mixer ellipse on behind data for reference 
             pylab.plot(xx, yy, color = 'firebrick', zorder = 0)
             cbar = pylab.colorbar()
             cbar.set_label('elapsed time (s)', rotation=270)
@@ -277,25 +301,26 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
             ax.yaxis.set_ticks_position('left')
             ax.set_aspect('equal')
             
-            ax.set_xlim([-0.16, 0.16])
-            ax.set_ylim([-0.16, 0.16])
+            ax.set_xlim([-xmax, xmax])
+            ax.set_ylim([-ymax, ymax])
     
+            #Plot phase data on standard phase vs t plot
             ax = pylab.subplot(1,2,2)
             pylab.plot(actualTimes[0:tind], Angles[0:tind], color = 'mediumblue', linestyle = '', marker = 'd', markersize = 3)
             pylab.xlabel('Time (s)')
             pylab.ylabel('Homodyne Phase (degrees)')
+            pylab.suptitle(figtitle)
             
-            pylab.suptitle('Homodyne Stability v. Time')
-            
-    #        pylab.tight_layout()
             fig1.canvas.draw()
             fig1.canvas.flush_events()
     
-    
-    
-    fig1 = pylab.figure(1, figsize=(14, 8))
+    ## Don't need this any more in theory:
+    '''
+    fig1 = pylab.figure(1, figsize=figure_size)
     pylab.clf()
     ax = pylab.subplot(1,2,1)
+
+    #Plot I/Q data on parametric plot with mixer ellipse trace underneath
     cVec = actualTimes
     cmap = 'cividis'
     pylab.scatter(Is, Qs, c = cVec, cmap = cmap , marker = 'o', s = 75,
@@ -317,26 +342,22 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
     ax.yaxis.set_ticks_position('left')
     ax.set_aspect('equal')
     
-    ax.set_xlim([-0.16, 0.16])
-    ax.set_ylim([-0.16, 0.16])
+    ax.set_xlim([-xmax, xmax])
+    ax.set_ylim([-ymax, ymax])
     
-    
+    #Phase vs time plot 
     ax = pylab.subplot(1,2,2)
     pylab.plot(actualTimes, Angles, color = 'mediumblue', linestyle = '', marker = 'd', markersize = 3)
     pylab.xlabel('Time (s)')
     pylab.ylabel('Homodyne Phase (degrees)')
-    date  = datetime.now()
-    stamp = date.strftime('%Y%m%d_%H_%M_%S')
-    filename = 'CWHomodyne{}{}{}{}_{}'.format(reference_signal, reference_freq_MHz, coupling_type,SGS_ref_freq, stamp)
-    fullpath = savepath+'\\'+filename
-    figtitle = 'Homodyne Stability v. Time\n Reference Signal: {}, Ref frequency: {}, Coupling:{}, SGS ref freq:{}\n Path:{}'.format(reference_signal,reference_freq_MHz,coupling_type, SGS_ref_freq, fullpath)
     
     pylab.suptitle(figtitle)
     
-    #        pylab.tight_layout()
     fig1.canvas.draw()
     fig1.canvas.flush_events()
-    
+    '''
+
+    #Save figure in both .pkl and .png format for easy viewing 
     uf.savefig(fig1,filename,savepath)
     uf.savefig(fig1,filename,savepath, png = True)
     
@@ -345,11 +366,16 @@ def CWHomodyneStabilityTest(ref='HDAWG', ref_freq=10, SGS_ref_freq=1000, couplin
     rfgen.power_Off()
     logen.power_Off()    
     
+###########################################################################################################
+# Example useage of CWHomodyneStabilityTest
+###########################################################################################################
+
 global card
 global hdawg
 global rfgen
 global logen
 
+# Check and see whether instruments have already been defined, if not, create new instances
 try:
     card.samples = 1024
 except:
@@ -377,18 +403,4 @@ CWHomodyneStabilityTest(ref = reference_signal, ref_freq = reference_freq_MHz, S
 #CWHomodyneStabilityTest(ref = reference_signal, ref_freq = reference_freq_MHz, SGS_ref_freq = SGS_ref_freq, coupling='Ref',measure_time = measure_time, savepath=savepath)
 #SGS_ref_freq       = 1000
 #CWHomodyneStabilityTest(ref = reference_signal, ref_freq = reference_freq_MHz, SGS_ref_freq = SGS_ref_freq, coupling='Ref',measure_time = measure_time, savepath=savepath)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
