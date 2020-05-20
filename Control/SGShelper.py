6c986c4202b2a82681828609a4d46d3afeb07c25
Kollar_Control2 <akollar@umd.edu>
2020-05-20 17:24:08 -0400

    Added helper function to use HDAWG channels as sine generators

--- Control/SGShelper.py
+++ Control/SGShelper.py
@@ -1,4 +1,3 @@
-
 def SGS_coupling(logen, rfgen, ext_ref='HDAWG', ext_ref_freq=10, coupling='Ref', SGS_ref_freq=1000):
     if ext_ref == 'HDAWG':
         rfgen.set_External_Reference(freq = ext_ref_freq)
@@ -14,3 +13,10 @@ def SGS_coupling(logen, rfgen, ext_ref='HDAWG', ext_ref_freq=10, coupling='Ref',
         logen.set_Internal_LO()
     if coupling == 'LO':
         logen.set_External_LO()
+
+def HDAWG_clock(hdawg, freqs=[], channels=[], amps=[]):
+    for freq, channel, amp in zip(freqs, channels, amps):
+        # Configure artificial clock using HDAWG
+        hdawg.OSCs[1].configure_sine(channel%2,freq)
+        hdawg.Channels[channel].analog_outs = [(channel-1)%2,channel%2]
+        hdawg.Channels[channel].configureChannel(amp=amp)
