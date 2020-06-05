fb112cdd598ab45d435f87a8d7090b867032cb9d
Martin Ritter <maritter@umd.edu>
2020-06-05 12:19:36 -0400

    WIP: added function to get dictionnary of settings for CWHomodyne measurement

---
+++ Control/CalibrationSuiteTests.py
@@ -0,0 +1,20 @@
+from Instruments.HDAWG import HDAWG
+#from Instruments.Acqiris import Acqiris
+from Instruments.SGS import RFgen
+
+from calibration import cwhomodynestability
+#card  = Acqiris('PXI23::0::0::INSTR')
+#hdawg = HDAWG('dev8163')
+#logen = RFgen('TCPIP0::rssgs100a110738::inst0::INSTR')
+#rfgen = RFgen('TCPIP0::rssgs100a110739::inst0::INSTR')
+#
+instruments = {}
+#instruments['AWG'] = hdawg
+#instruments['Digitizer'] = card
+#instruments['LO'] = logen
+#instruments['RFgen'] = rfgen
+
+defaults = cwhomodynestability.GetDefaultSettings()
+settings = defaults
+print(settings)
+#CWHomodyneStabilityTest(instruments, settings)
