76bc9944d61f322be1c7719d8206faa3fa0f98f2
Martin Ritter <maritter@umd.edu>
2020-06-08 16:57:34 -0400

    Removed the IQ mixer calibration function from file and cleaned up some old versions of code

--- Control/Calibration/cwhomodynestability.py
+++ Control/Calibration/cwhomodynestability.py
@@ -20,117 +20,6 @@ import userfuncs as uf
 from SGShelper import HDAWG_clock, SGS_coupling
 
 
-def calibrate_mixer_IQ(freq, power, numPoints, measDur = 5e-6, verbose = False, showFig = True):
-    '''
-    Helper function to create IQ imbalance ellipse. Returns the axes, center and angle of the ellipse
-    Can draw the ellipse if desired
-    Arguments:
-        freq (int): frequency used for LO/RF generator
-        power (double): power (in dB) for RF generator
-        numPoints (int): number of points to take on the ellipse
-        measDur (double): time for digitizer to acquire dataset (default 5e-6)
-        verbose (bool): sets verbosity of output (default False)
-        showFig (bool): sets whether we draw a figure or not (default True)
-    Returns:
-        axes ([double]): array with the x and y axes of the ellipse
-        center ([double]): array with the coordinates of the ellipse
-        phi (double): angle of the ellipse (45 degrees means a circle in this case)
-    '''
-    
-    freq_GHz = freq/1e9
-    phases = numpy.linspace(0,360,numPoints)
-
-    ## Digitizer card settings 
-    card.triggerSlope = 'Rising'
-    card.triggerLevel = 0.1
-    card.averages = 1 #on-board averages
-    card.segments = 1
-    card.triggerDelay = 0
-    card.activeChannels = [1,2]
-    card.verbose = False
-    card.sampleRate = 2e9
-    card.clockSource = 'External'
-    card.channelRange = 0.5
-    card.samples = numpy.ceil(measDur*card.sampleRate)
-    card.SetParams() #warning. this may round the number of smaples to multiple of 1024
-    
-    ## SGS unit settings
-    logen.set_Freq(freq_GHz)
-    logen.set_Amp(12)
-    logen.mod_Off()
-    logen.power_On() 
-    
-    rfgen.set_Freq(freq_GHz)
-    rfgen.set_Amp(power)
-    rfgen.mod_Off()
-    rfgen.power_On()
-
-    ## Wait for settings to percolate
-    time.sleep(0.5)
-    
-    Idata = numpy.zeros(card.samples)
-    Qdata = numpy.zeros(card.samples)
-    
-    Amps = numpy.zeros(numPoints)
-    Angles = numpy.zeros(numPoints)
-    Is = numpy.zeros(numPoints)
-    Qs = numpy.zeros(numPoints)
-    
-    for tind in range(0, numPoints):
-        logen.set_Phase(phases[tind])
-        time.sleep(0.05)
-        
-        card.ArmAndWait()
-        Idata, Qdata = card.ReadAllData()
-        
-        Iav = numpy.mean(Idata)
-        Qav = numpy.mean(Qdata)
-        
-        Amp = numpy.sqrt(Iav**2 + Qav**2)
-        Angle = numpy.arctan2(Iav, Qav)*180/numpy.pi
-        
-        Amps[tind] = Amp
-        Angles[tind] = Angle
-        
-        Is[tind] = Iav
-        Qs[tind] = Qav
-        
-    axes, center, phi = uf.fitEllipse(Is,Qs, verbose = True)
-    
-    if showFig:
-        xx, yy = uf.make_elipse(axes,  center, phi, 150)
-        
-        fig = pylab.figure(10)
-        pylab.clf()
-        ax = pylab.subplot(1,1,1)
-        pylab.plot(Is, Qs, linestyle = '', marker = 'o', markersize = 5, color = 'mediumblue')
-        pylab.plot(xx, yy, color = 'firebrick')
-        
-        
-        # Move left y-axis and bottim x-axis to centre, passing through (0,0)
-        ax.spines['left'].set_position('center')
-        ax.spines['bottom'].set_position('center')
-        
-        # Eliminate upper and right axes
-        ax.spines['right'].set_color('none')
-        ax.spines['top'].set_color('none')
-        
-        # Show ticks in the left and lower axes only
-        ax.xaxis.set_ticks_position('bottom')
-        ax.yaxis.set_ticks_position('left')
-        
-        
-        ax.set_aspect('equal')
-        titleStr = 'Mixer performance '
-        pylab.title(titleStr)
-    #    pylab.show(block = False)
-        
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-    
-    
-    return axes, center, phi
-
 def CWHomodyneStabilityTest(instruments, settings):
     '''
     General useage function that performs a homodyne phase stability measurement. Produces a figure showing mixer
@@ -157,11 +46,11 @@ def CWHomodyneStabilityTest(instruments, settings):
     
     ## Measurement parameters
     measure_time = settings['measure_time']
-    measDur = settings['one_shot_time']
-    freq = settings['frequency']
-    rfpower = settings['rfpower']
-    lopower = settings['lopower']             
-    freq_GHz = freq/1e9
+    measDur      = settings['one_shot_time']
+    freq         = settings['frequency']
+    rfpower      = settings['rfpower']
+    lopower      = settings['lopower']             
+    freq_GHz     = freq/1e9
 
     ## Misc settings
     savepath = settings['savepath']
@@ -171,12 +60,6 @@ def CWHomodyneStabilityTest(instruments, settings):
     channels = [2,3]
     amps = [1,2]
     HDAWG_clock(hdawg, freqs, channels, amps)
-    #hdawg.Channels[2].analog_outs = [0.5,0] #set output amplitudes of sine generator
-    #hdawg.Channels[3].analog_outs = [0,1] 
-    #hdawg.Channels[2].configureChannel(amp=1.0) #set output range of sine generator
-    #hdawg.Channels[3].configureChannel(amp=2.0)
-    #hdawg.OSCs[1].configure_sine(0,10e6) #set frequency of sine generator
-    #hdawg.OSCs[1].configure_sine(1,reference_freq_MHz*1e6)
     
     logen.power_Off()
     rfgen.power_Off()
@@ -215,23 +98,6 @@ def CWHomodyneStabilityTest(instruments, settings):
 
     rfgen.power_On()
     logen.power_On()
-    ## Apply reference/ coupling that the user has set up
-    #if reference_signal == 'HDAWG':
-    #    rfgen.set_External_Reference(freq = reference_freq_MHz)
-    #else:
-    #    rfgen.set_Internal_Reference()
-    #if coupling_type == 'LO':
-    #    rfgen.set_RefLO_output(output='LO')
-    #else:
-    #    rfgen.set_RefLO_output(output='Ref', freq = SGS_ref_freq)
-    #rfgen.power_On()
-    
-    #if coupling_type == 'Ref':
-    #    logen.set_External_Reference(freq = SGS_ref_freq)
-    #    logen.set_Internal_LO()
-    #if coupling_type == 'LO':
-    #    logen.set_External_LO()
-    #logen.power_On() 
     
     # Wait and make sure that the settings have been applied
     #TO DO: write SGS function to check PLL status to avoid these long waits
