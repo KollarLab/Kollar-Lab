344582e6f92e48c746d7ce82d735b601ec5fc5dc
Kollar_Control2 <akollar@umd.edu>
2020-06-11 15:25:26 -0400

    Updated code to use HDAWG as clock in initialize step and added HW address for trigger generator

--- Control/InitializeHardware.py
+++ Control/InitializeHardware.py
@@ -16,7 +16,7 @@ from Instruments.HDAWG import HDAWG
 from Instruments.Acqiris import Acqiris
 from Instruments.SGS import RFgen
 from Instruments.Generator import Generator
-from SGShelper import SGS_coupling
+from SGShelper import SGS_coupling, HDAWG_clock
 
 #Digitizer
 hardwareAddress = "PXI23::0::0::INSTR"
@@ -24,21 +24,25 @@ card = Acqiris(hardwareAddress)
 
 #HDAWG
 hdawg = HDAWG('dev8163')
+freqs = [10e6,10e6]
+channels = [2,3]
+amps = [1,2]
+HDAWG_clock(hdawg, freqs, channels, amps)
 
 #Generators
 logen = RFgen('TCPIP0::rssgs100a110738::inst0::INSTR')
 rfgen = RFgen('TCPIP0::rssgs100a110739::inst0::INSTR')
 SGS_coupling(logen,rfgen)
 
-triggergen  = Generator('')
-syncgen     = Generator('')
+triggergen  = Generator('USB0::0x0957::0x2507::MY58000681::0::INSTR')
+#syncgen     = Generator('')
 
 triggergen.reference = 'Ext'
 triggergen.waveform  = 'SQU'
 triggergen.frequency = '500 Hz'
 triggergen.volts     = '0.5 V'
 
-syncgen.reference = 'Ext'
-syncgen.waveform  = 'SIN'
-syncgen.frequency = '10 MHz'
-syncgen.volts     = '3 dBm'
+#syncgen.reference = 'Ext'
+#syncgen.waveform  = 'SIN'
+#syncgen.frequency = '10 MHz'
+#syncgen.volts     = '3 dBm'
