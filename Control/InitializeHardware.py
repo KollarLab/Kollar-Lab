f8310cebcb97f2b4da681f8ab5d3db7cb3ae0d4b
Kollar_Control2 <akollar@umd.edu>
2020-06-17 13:46:10 -0400

    Updated init HW code to use external Rb clock and fixed triggering issue

--- Control/InitializeHardware.py
+++ Control/InitializeHardware.py
@@ -24,10 +24,6 @@ card = Acqiris(hardwareAddress)
 
 #HDAWG
 hdawg = HDAWG('dev8163')
-freqs = [10e6,10e6]
-channels = [2,3]
-amps = [1,2]
-HDAWG_clock(hdawg, freqs, channels, amps)
 
 #Generators
 logen = RFgen('TCPIP0::rssgs100a110738::inst0::INSTR')
@@ -35,15 +31,9 @@ rfgen = RFgen('TCPIP0::rssgs100a110739::inst0::INSTR')
 SGS_coupling(logen,rfgen)
 
 triggergen  = Generator('USB0::0x0957::0x2507::MY58000681::0::INSTR')
-#syncgen     = Generator('')
 
 triggergen.reference = 'Ext'
 triggergen.waveform  = 'SQU'
 triggergen.frequency = '500 Hz'
-triggergen.volts     = '0.5 V'
+triggergen.volts     = '2 V'
 triggergen.output    = 'ON'
-
-#syncgen.reference = 'Ext'
-#syncgen.waveform  = 'SIN'
-#syncgen.frequency = '10 MHz'
-#syncgen.volts     = '3 dBm'
