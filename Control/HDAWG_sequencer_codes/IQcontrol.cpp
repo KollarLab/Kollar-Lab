3c87313303091be94f1c0e3c67b26eb34965a7a5
Kollar_Control2 <akollar@umd.edu>
2020-06-29 17:35:36 -0400

    Fixed syntax, missing ;

--- Control/HDAWG_sequencer_codes/IQcontrol.cpp
+++ Control/HDAWG_sequencer_codes/IQcontrol.cpp
@@ -1,12 +1,12 @@
-const Angle     = _angle_
-const Time      = _time_
+const Angle     = _angle_;
+const Time      = _time_;
 
 const sampleRate      = 2.4e+9;
 const sequencerRate   = sampleRate/8;
 const Time_samples    = round(Time*sampleRate/16)*16;
 
-wave w1 = ones(Time_samples)
+wave w1 = ones(Time_samples);
 
 while(true){
-    playWave(w1*Cos(Angle), w1*Sin(Angle))
+    playWave(w1*cos(Angle), w1*sin(Angle));
 }
