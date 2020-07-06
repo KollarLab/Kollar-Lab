55f0ddd625dd0ad5899851e9e268043c0f4fc641
Kollar_Control2 <akollar@umd.edu>
2020-07-06 19:08:13 -0400

    Moved main code to loop so that it can be easily iterated, made reduced version that only computes eccentricity to plot on a color map of angle impairment vs amplitude impairment

--- Control/testingMixervsSGSIQ.py
+++ Control/testingMixervsSGSIQ.py
@@ -9,6 +9,8 @@ import Calibration.sgsIQcal as SGSiq
 import Calibration.mixerIQcal as mixeriq
 import userfuncs as uf
 import pylab
+import numpy
+from datetime import datetime
 
 instruments = {}
 instruments['AWG'] = hdawg
@@ -16,35 +18,73 @@ instruments['Digitizer'] = card
 instruments['LO'] = logen
 instruments['RFgen'] = rfgen
 
-settings = mixeriq.GetDefaultSettings()
-settings['NumPoints'] = 50
+settings = SGSiq.GetDefaultSettings()
 
-SGSa, SGSc, SGSp = SGSiq.calibrate_SGS_IQ(instruments, settings)
-mixera, mixerc, mixerp = mixeriq.calibrate_mixer_IQ(instruments, settings)
+def runIQcal(instruments, settings, angle, imp):
+    settings['NumPoints'] = 100
+    settings['AngleImp']  = angle
+    settings['AmpImp']    = imp
+    
+    SGSa,SGSc,SGSp,SGSe = SGSiq.calibrate_SGS_IQ(instruments, settings)
+    
+    SGSxx, SGSyy = uf.make_elipse(SGSa, SGSc, SGSp,150)
+    
+    maxSGSx = abs(max(SGSxx))
+    maxSGSy = abs(max(SGSyy))
+    maxdim  = max([maxSGSx,maxSGSy])
+    
+    figtitle = 'IQ ellipse relative to perfect circle.\n Alpha: {}, Phi: {}'.format(settings['AmpImp'], settings['AngleImp'])
+    myfig = pylab.figure(1)
+    pylab.clf()
+    pylab.plot(SGSxx, SGSyy, color='r')
+    
+    circle2 = pylab.Circle((SGSc[0], SGSc[1]), maxdim, color='b', fill=False)
+    ax = pylab.gca()
+    ax.add_artist(circle2)
+    ax.set_xlim([-1.1*maxdim, 1.1*maxdim])
+    ax.set_ylim([-1.1*maxdim, 1.1*maxdim])
+    ax.set_title(figtitle)
+    ax.set_aspect('equal')
+    
+    date  = datetime.now()
+    stamp = date.strftime('%Y%m%d_%H%M%S')
+    
+    figname = 'IQcal_{}'.format(stamp)
+    figpath = r'C:\Users\Kollarlab\Desktop\IQcal'
+    uf.savefig(myfig, figname, figpath, png=True)
 
-SGSxx, SGSyy = uf.make_elipse(SGSa, SGSc, SGSp,150)
-mixerxx, mixeryy = uf.make_elipse(mixera, mixerc, mixerp, 150)
+def IQcal(instruments, settings, angle, imp):
+    settings['NumPoints'] = 100
+    settings['AngleImp']  = angle
+    settings['AmpImp']    = imp
+    settings['verbose']   = False
+    
+    SGSa, SGSc, SGSp, SGSe = SGSiq.calibrate_SGS_IQ(instruments, settings)
+    return SGSe
 
-maxSGSx = abs(max(SGSxx))
-maxSGSy = abs(max(SGSyy))
-maxMixerx = abs(max(mixerxx))
-maxMixery = abs(max(mixeryy))
+angles  = numpy.linspace(-0.10,.025,num = 20)
+impairs = numpy.linspace(0.87,.93,num = 20)
 
-normSGSx = SGSxx/maxSGSx
-normSGSy = SGSyy/maxSGSy
-normMixerx = mixerxx/maxMixerx
-normMixery = mixeryy/maxMixery
+ecc = numpy.zeros((len(angles), len(impairs)))
+for i in range(len(angles)):
+    print('{} out of {} angle measurements, {} amp measures per setting'.format(i, len(angles), len(impairs)))
+    for j in range(len(impairs)):
+        if j%5 == 0:
+            print('{} measurements completed'.format(j))
+        ecc[i,j] = IQcal(instruments, settings, angles[i], impairs[j])
 
-pylab.figure(1)
-pylab.plot(SGSxx, SGSyy)
-pylab.plot(mixerxx, mixeryy)
-pylab.title('Raw comparison between IQ mod on SGS and phase control on SGS')
-pylab.legend(['SGS','Mixer'])
-pylab.show()
+saveMe = pylab.figure(1)
+pylab.imshow(ecc, origin = 'lower', extent = [impairs[0], impairs[-1], angles[0], angles[-1]])
+pylab.colorbar()
+ax = pylab.gca()
+ax.set_ylabel('Angles')
+ax.set_xlabel('Amplitudes')
+ax.set_title('Eccentricity vs angle impairement and amplitude impairement')
 
-pylab.figure(2)
-pylab.plot(normSGSx, normSGSy)
-pylab.plot(normMixerx, normMixery)
-pylab.title('Normalized traces')
-pylab.legend(['SGS','Mixer'])
-pylab.show()
+date  = datetime.now()
+stamp = date.strftime('%Y%m%d_%H%M%S')
+
+filename = 'IQcal_{}points_{}'.format(len(angles)*len(impairs),stamp)
+path = r'C:\Users\Kollarlab\Desktop\IQcal'
+uf.SaveFull(path, filename, 'ecc', locals(), settings, instruments, saveMe)
+uf.savefig(saveMe, filename, path, png=True)
