e8d9360ac0876d88903dd0c94e4b233546aaf177
Kollar_Control2 <akollar@umd.edu>
2020-07-06 19:04:40 -0400

    Code to run through a full IQ ellipse at a given angle and amplitude impairment

--- Control/HDAWG_sequencer_codes/IQcontrol.cpp
+++ Control/HDAWG_sequencer_codes/IQcontrol.cpp
@@ -1,12 +1,20 @@
-const Angle     = _angle_;
 const Time      = _time_;
+const NumPoints = _npoints_;
+const Phi       = _angimp_;
+const AlphaI    = _ampimpI_;
+const AlphaQ    = _ampimpQ_;
 
 const sampleRate      = 2.4e+9;
 const sequencerRate   = sampleRate/8;
 const Time_samples    = round(Time*sampleRate/16)*16;
 
-wave w1 = ones(Time_samples);
+wave w1   = ones(Time_samples);
+wave sin  = AlphaQ*sine(NumPoints,1.0,Phi,2.);
+wave cos  = AlphaI*cosine(NumPoints,1.0,0.,2.);
 
 while(true){
-    playWave(w1*cos(Angle), w1*sin(Angle));
+  for (cvar i = 0; i < NumPoints; i++) {
+    waitDigTrigger(1);
+    playWave(w1*cos[i],w1*sin[i]);
+  }
 }
