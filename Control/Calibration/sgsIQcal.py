aa972c5944f34d161a78819f12012a7aec193c5d
Kollar_Control2 <akollar@umd.edu>
2020-07-06 19:07:07 -0400

    Updated code to collect full IQ sweep in one read and add option to control IQ impairments

--- Control/Calibration/sgsIQcal.py
+++ Control/Calibration/sgsIQcal.py
@@ -7,7 +7,9 @@ def GetDefaultSettings():
     settings['lopower']        = 12
     settings['rfpower']        = 0
     settings['NumPoints']      = 30
-    settings['SingleShotTime'] = 5e-6
+    settings['SingleShotTime'] = 10e-6
+    settings['AngleImp']       = 0
+    settings['AmpImp']         = 1.
     settings['verbose']        = False
     settings['showFig']        = False
     return settings
@@ -23,37 +25,65 @@ def calibrate_SGS_IQ(instruments, settings):
     lopower   = settings['lopower']
     rfpower   = settings['rfpower']
     numPoints = settings['NumPoints']
+    angle_impairment = settings['AngleImp']
+    amp_impairment   = settings['AmpImp']
     measDur   = settings['SingleShotTime']
-    verbose   = settings['verbose']
     showFig   = settings['showFig']
+    verbose   = settings['verbose']
 
     freq_GHz = freq/1e9
-    phases = numpy.linspace(0,2*numpy.pi,numPoints)
-
+    #phases = numpy.linspace(0,2*numpy.pi,numPoints)
+    amp_impairment_Q = 1.
+    amp_impairment_I = 1.
+    
+    if amp_impairment>1:
+        amp_impairment_Q = 1./amp_impairment
+        if verbose:
+            print('Q impairment: {}'.format(amp_impairment_Q))
+    else:
+        amp_impairment_I = amp_impairment
+        if verbose:
+            print('I impairment: {}'.format(amp_impairment_I))
+    if verbose:
+        print('Angle impairment: {}'.format(angle_impairment))
     ## HDAWG settings
+        print('HDAWG config')
     hdawg.AWGs[0].samplerate = '2.4GHz'
     hdawg.channelgrouping = '1x4'
     hdawg.Channels[0].configureChannel(amp=1.0,marker_out='Marker', hold='False')
     hdawg.Channels[1].configureChannel(amp=1.0,marker_out='Marker', hold='False')
+    hdawg.AWGs[0].Triggers[0].configureTrigger(slope='rising',channel='Trigger in 1')
+    if verbose:
+        print('HDAWG config ended')
     progFile = open("HDAWG_sequencer_codes/IQcontrol.cpp",'r')
     rawprog  = progFile.read()
     loadprog = rawprog
     progFile.close()
-    loadprog = loadprog.replace('_time_', str(measDur))
-
+    loadprog = loadprog.replace('_time_', str(1.1*measDur))
+    loadprog = loadprog.replace('_npoints_', str(numPoints))
+    loadprog = loadprog.replace('_angimp_', str(angle_impairment))
+    loadprog = loadprog.replace('_ampimpI_', str(amp_impairment_I))
+    loadprog = loadprog.replace('_ampimpQ_', str(amp_impairment_Q))
+    if verbose:
+        print('Program loading')
+    hdawg.AWGs[0].load_program(loadprog)
+    hdawg.AWGs[0].run_loop()
+    if verbose:
+        print('Program loaded')
+    
     ## Digitizer card settings 
     card.triggerSlope = 'Rising'
     card.triggerLevel = 0.1
     card.clockSource = 'External'
     card.verbose = False
     
-    card.triggerDelay = 0
+    card.triggerDelay = 0.2e-6
     card.activeChannels = [1,2]
     card.channelRange = 2.5
     card.sampleRate = 2e9
     
     card.averages = 1 #on-board averages
-    card.segments = 1
+    card.segments = numPoints
     card.samples = numpy.ceil(measDur*card.sampleRate)
     card.SetParams() #warning. this may round the number of smaples to multiple of 1024
     
@@ -71,26 +101,23 @@ def calibrate_SGS_IQ(instruments, settings):
     ## Wait for settings to percolate
     time.sleep(0.5)
     
-    Idata = numpy.zeros(card.samples)
-    Qdata = numpy.zeros(card.samples)
+    Idata = numpy.zeros((card.segments, card.samples))
+    Qdata = numpy.zeros((card.segments, card.samples))
     
     Amps = numpy.zeros(numPoints)
     Angles = numpy.zeros(numPoints)
     Is = numpy.zeros(numPoints)
     Qs = numpy.zeros(numPoints)
-    traces = numpy.zeros((numPoints, card.samples))
-    
+    if verbose:
+        print('Acquisition started')
+    card.ArmAndWait()
+    Idata, Qdata = card.ReadAllData()
+    if verbose:
+        print('Acquisition ended')
     for tind in range(0, numPoints):
-        finalprog = loadprog.replace('_angle_', str(phases[tind]))
-        hdawg.AWGs[0].load_program(finalprog)
-        hdawg.AWGs[0].run_loop()
-        time.sleep(0.05)
-        
-        card.ArmAndWait()
-        Idata, Qdata = card.ReadAllData()
-        
-        Iav = numpy.mean(Idata)
-        Qav = numpy.mean(Qdata)
+
+        Iav = numpy.mean(Idata[tind])
+        Qav = numpy.mean(Qdata[tind])
         
         Amp = numpy.sqrt(Iav**2 + Qav**2)
         Angle = numpy.arctan2(Iav, Qav)*180/numpy.pi
@@ -100,9 +127,8 @@ def calibrate_SGS_IQ(instruments, settings):
         
         Is[tind] = Iav
         Qs[tind] = Qav
-        traces[tind] = Idata
-    
-    axes, center, phi = uf.fitEllipse(Is,Qs, verbose = True)
+
+    axes, center, phi, ecc = uf.fit_ell_martin(Is,Qs, verbose)
     
     if showFig:
         xx, yy = uf.make_elipse(axes, center, phi, 150)
@@ -135,4 +161,4 @@ def calibrate_SGS_IQ(instruments, settings):
         fig.canvas.draw()
         fig.canvas.flush_events()
         
-    return axes, center, phi
+    return axes, center, phi, ecc
