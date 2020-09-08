44d3d57ea02254612e4df937f12b6bd6cf76c490
Kollar_Control2 <akollar@umd.edu>
2020-09-08 11:22:35 -0400

    WIP: core commands work now, trying to implement submodules for IQ and reference

--- Control/Instruments/RFsource.py
+++ Control/Instruments/RFsource.py
@@ -12,13 +12,34 @@ class RFsource(object):
     def __getattr__(self, name):
         cmds = self.__dict__['commandset']
         if name in cmds.keys():
-            return self.inst.query(cmds[name])
+            return self.inst.query(cmds[name]+'?')
         else:
-            print('Setting not defined: {}'.format(name))
+            super().__getattribute__(self, name)
     
     def __setattr__(self, name, value):
-        cmds = self.__dict__['commandset']
-        if name in cmds.keys():
-            self.inst.write(cmds['self'])
+        print('Called setattribute1')
+        cmds = {}
+        try:
+            cmds = self.__dict__['commandset']
+            configured = True
+        except:
+            print('Doin stupid')
+            super().__setattr__(name, value)
+        
+        if name in cmds.keys() and configured:
+            cmd = '{} {}'.format(cmds[name], value)
+            print(cmd)
+            self.inst.write(cmd)
         else:
-            super(RFsource, self).__setattr__(name, value)
+            print('Doin stupid')
+            super().__setattr__(name, value)
+            
+#class testsub(object):
+#
+#    def __init__(self,subname):
+#        self.subname = subname
+#    def __getattr__(self,name):
+#        print('Called getattribute2')
+#    def __setattr__(self, name, value):
+#        print('Called setattribute2')
+#        super().__setattr__(name, value)
