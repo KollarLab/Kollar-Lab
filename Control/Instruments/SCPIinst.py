768d2b936a72208f6b398199ad7ec82d7ce40fe5
Kollar_Control2 <akollar@umd.edu>
2020-09-15 16:23:16 -0400

    Added error checking after every SCPI command, user can now specify bidicts to translate return/set values into more informative strings (e.g. convert a 0 to "On")

--- Control/Instruments/SCPIinst.py
+++ Control/Instruments/SCPIinst.py
@@ -2,31 +2,40 @@ import pyvisa
 
 class SCPIinst(object):
 
-    def __init__(self, address, commands):
+    def __init__(self, address, commands, errcmd):
         rm = pyvisa.ResourceManager()
         self.inst = rm.open_resource(address)
-        self.inst.write('*RST')
+        self.inst.write('*RST; *CLS')
+        self.errcmd = errcmd
         self.modules = []
         for key in commands.keys():
             if key == 'core':
                 self.commandset = commands['core']
             elif isinstance(commands[key],dict):
-                setattr(self,key,Module(self.inst, commands[key]))
+                setattr(self,key,Module(self.inst, commands[key], errcmd))
                 self.modules.append(key)
     
     def __getattr__(self, name):
         cmds = self.__dict__['commandset']
         if name in cmds.keys():
-            val = self.inst.query(cmds[name]+'?').rstrip()
-            try:
-                return eval(val)
-            except:
-                return val
+            if isinstance(cmds[name], list):
+                val = self.inst.query(cmds[name][0]+'?').rstrip()
+                try:
+                    val = eval(val)
+                    strings = cmds[name][1]
+                    return strings[val]
+                except:
+                    return val
+            else:
+                val = self.inst.query(cmds[name]+'?').rstrip()
+                try:
+                    return eval(val)
+                except:
+                    return val
         else:
             super().__getattribute__(self, name)
     
     def __setattr__(self, name, value):
-#        print('Called setattribute1')
         cmds = {}
         try:
             cmds = self.__dict__['commandset']
@@ -34,13 +43,50 @@ class SCPIinst(object):
             pass 
         
         if name in cmds.keys():
-            cmd = '{} {}'.format(cmds[name], value)
-#            print(cmd)
+            setting = cmds[name]
+            if isinstance(setting, list):
+                command = setting[0]
+                try:
+                    value = setting[1].inverse[value]
+                except:
+                    print('Invalid input, valid options are: {}'.format(list(setting[1].inverse.keys())))
+            else:
+                command = setting
+            cmd = '{} {}'.format(command, value)
             self.inst.write(cmd)
-            self.inst.write('*OPC?')
+            self.inst.write('*OPC')
+            self.error
         else:
-#            print('Doin stupid')
             super().__setattr__(name, value)
+    
+    def close(self):
+        self.inst.close()
+        
+    @property
+    def error(self):
+        errcmd = self.errcmd
+        if isinstance(errcmd, dict):
+            for errtype in errcmd.keys():
+                code = 0
+                string = {}
+                error_struct = errcmd[errtype]
+                if isinstance(error_struct, list):
+                    errorstring = self.inst.query(errcmd[errtype][0])
+                    errstr = errcmd[errtype][1]
+                else:
+                    errorstring = self.inst.query(errcmd[errtype])
+                try:
+                    (code, string) = eval(errorstring)
+                except:
+                    code = eval(errorstring)
+                    string = errstr[code]
+                if code != 0:
+                    print('{}:{},{}'.format(errtype, code, string))
+        else:
+            (code, string) = eval(self.inst.query(errcmd))
+            if code != 0:
+                print('{},{}'.format(code, string))
+        
     @property
     def settings(self):
         fullsettings = {}
@@ -65,23 +111,32 @@ class SCPIinst(object):
             
 class Module(object):
 
-    def __init__(self,inst, commands):
+    def __init__(self,inst, commands, errcmd):
         self.inst = inst
         self.commandset = commands
+        self.errcmd = errcmd
         
     def __getattr__(self, name):
         cmds = self.__dict__['commandset']
         if name in cmds.keys():
-            val = self.inst.query(cmds[name]+'?').rstrip()
-            try:
-                return eval(val)
-            except:
-                return val
+            if isinstance(cmds[name], list):
+                val = self.inst.query(cmds[name][0]+'?').rstrip()
+                try:
+                    val = eval(val)
+                    strings = cmds[name][1]
+                    return strings[val]
+                except:
+                    return val
+            else:
+                val = self.inst.query(cmds[name]+'?').rstrip()
+                try:
+                    return eval(val)
+                except:
+                    return val
         else:
             super().__getattribute__(self, name)
     
     def __setattr__(self, name, value):
-#        print('Called setattribute1')
         cmds = {}
         try:
             cmds = self.__dict__['commandset']
@@ -89,13 +144,47 @@ class Module(object):
             pass 
         
         if name in cmds.keys():
-            cmd = '{} {}'.format(cmds[name], value)
-#            print(cmd)
+            setting = cmds[name]
+            if isinstance(setting, list):
+                command = setting[0]
+                try:
+                    value = setting[1].inverse[value]
+                except:
+                    print('Invalid input, valid options are: {}'.format(list(setting[1].inverse.keys())))
+            else:
+                command = setting
+            cmd = '{} {}'.format(command, value)
             self.inst.write(cmd)
-            self.inst.write('*OPC?')
+            self.inst.write('*OPC')
+            self.error
         else:
-#            print('Doin stupid')
             super().__setattr__(name, value)
+            
+    @property
+    def error(self):
+        errcmd = self.errcmd
+        if isinstance(errcmd, dict):
+            for errtype in errcmd.keys():
+                code = 0
+                string = {}
+                error_struct = errcmd[errtype]
+                if isinstance(error_struct, list):
+                    errorstring = self.inst.query(errcmd[errtype][0])
+                    errstr = errcmd[errtype][1]
+                else:
+                    errorstring = self.inst.query(errcmd[errtype])
+                try:
+                    (code, string) = eval(errorstring)
+                except:
+                    code = eval(errorstring)
+                    string = errstr[code]
+                if code != 0:
+                    print('{}:{},{}'.format(errtype, code, string))
+        else:
+            (code, string) = eval(self.inst.query(errcmd))
+            if code != 0:
+                print('{},{}'.format(code, string))
+                
     @property
     def settings(self):
         fullsettings = {}
