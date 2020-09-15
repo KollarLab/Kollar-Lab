e429db4034e6dfdb9506285fdc60765c747d8697
Kollar_Control2 <akollar@umd.edu>
2020-09-15 17:47:56 -0400

    Fixed typo and closed all VISA connections before initializing hardware to avoid getting errors trying to establish a connection to an instrument that already has a connection

--- Control/InitializeHardware.py
+++ Control/InitializeHardware.py
@@ -4,19 +4,14 @@ Created on Wed May  6 14:39:58 2020
 
 @author: Kollarlab
 """
-
-import pylab
-import sys
-import numpy
-import scipy
-import time
-from mpldatacursor import datacursor
+import pyvisa
 
 from Instruments.HDAWG import HDAWG
 from Instruments.Acqiris import Acqiris
 from Instruments.SGS import SGS100A
 from Instruments.Generator import Keysight33500B
 from Instruments.DC205 import DC205
+
 from SGShelper import SGS_coupling, HDAWG_clock
 
 #Digitizer
@@ -27,6 +22,10 @@ card = Acqiris(hardwareAddress)
 hdawg = HDAWG('dev8163')
 
 #Generators
+
+rm = pyvisa.ResourceManager()
+rm.close()
+
 logen = SGS100A('TCPIP0::rssgs100a110738::inst0::INSTR')
 rfgen = SGS100A('TCPIP0::rssgs100a110739::inst0::INSTR')
 SGS_coupling(logen,rfgen)
@@ -36,7 +35,7 @@ triggergen  = Keysight33500B('USB0::0x0957::0x2507::MY58000681::0::INSTR')
 triggergen.Ref.Source = 'Ext'
 triggergen.Waveform   = 'SQU'
 triggergen.Freq       = '500 Hz'
-triggergen.Volts      = '2 V'
+triggergen.Volt       = '2 V'
 triggergen.Output     = 'ON'
 
 SRS = DC205('ASRL3::INSTR')
