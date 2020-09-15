5a7ef9797ee97a76e1457470775fc710b8e2e6cf
Kollar_Control2 <akollar@umd.edu>
2020-09-15 10:48:44 -0400

    Updated control code to use more general SCPIinst class structure

--- Control/Instruments/Generator.py
+++ Control/Instruments/Generator.py
@@ -1,6 +1,32 @@
 import pyvisa
 from userfuncs import freeze
+from Instruments.SCPIinst import SCPIinst
 
+class Keysight33500B(SCPIinst):
+    commandlist = {}
+    commandlist['core']  = {}
+    commandlist['Pulse'] = {}
+    
+    core = {}
+    core['Freq']     = 'FREQuency'
+    core['Volt']     = 'VOLTage'
+    core['Offset']   = 'VOLTage:OFFSet'
+    core['Waveform'] = 'FUNCtion'
+    core['Output']   = 'OUTPut'
+    
+    pulse = {}
+    pulse['Width'] = 'FUNCtion:PULSe:WIDTh'
+    
+    ref = {}
+    ref['Source'] = 'ROSCillator:SOURce'
+    
+    commandlist['core']  = core
+    commandlist['Pulse'] = pulse
+    commandlist['Ref']   = ref
+    
+    def __init__(self, address):
+        super().__init__(address, self.commandlist)
+    
 @freeze
 class Generator():
 
