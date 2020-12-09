8fc8ce466c9359c72b3e08310df7ceb7919b8955
Martin Ritter <maritter@umd.edu>
2020-12-09 11:23:45 -0500

    Driver scripts that run T1 and pulsedspec functions for user defined settings

---
+++ Scripts/PulsedSpecDriver.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 19:47:15 2020
+
+@author: Kollarlab
+"""
+
+from CleanPulsedspec import GetDefaultSettings, PulsedSpec
+
+instruments = {}
+instruments['cavitygen'] = cavitygen
+instruments['qubitgen'] = qubitgen
+instruments['LO'] = SMB
+instruments['card'] = card
+instruments['AWG'] = hdawg
+
+settings = GetDefaultSettings()
+settings['scanname'] = 'q4_PulseLengthSweep_5ns_widersweep'
+settings['saveDir']  = r'Z:\Data\HouckQuadTransmon\PulsedSpec\20201208'
+
+#Cavity parameters
+settings['CAVpower']        = -18
+settings['CAV_freq']        = 8.126e9
+
+#Qubit parameters
+q_freq = 4.20554796e9
+span = 200e6
+settings['start_freq']      = q_freq-span/2 
+settings['stop_freq']       = q_freq+span/2
+settings['freq_points']     = 30
+
+settings['start_power']     = 5
+settings['stop_power']      = 20
+settings['power_points']    = 31
+
+#Card settings
+settings['segments']         = 1
+settings['reads']            = 1
+settings['averages']         = 5e3
+settings['activeChannels']   = [1,2]
+settings['channelRange']     = 0.5
+settings['sampleRate']       = 2e9/8
+settings['meas_window']      = 10e-6
+settings['timeout']          = 30
+
+##Pulse settings
+settings['meas_pos'] = 80e-6
+settings['pulse_delay'] = 200e-9
+settings['pulse_width'] = 10e-9
+
+PulsedSpec(instruments, settings)
---
+++ Scripts/T1Driver.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 16:42:56 2020
+
+@author: Kollarlab
+"""
+import time
+
+from T1WIP import GetDefaultSettings, meas_T1
+
+instruments = {}
+instruments['cavitygen'] = cavitygen
+instruments['qubitgen'] = qubitgen
+instruments['LO'] = SMB
+instruments['card'] = card
+instruments['AWG'] = hdawg
+
+settings = GetDefaultSettings()
+settings['scanname'] = 'T1_meas_stability'
+settings['project_dir'] = r'Z:\Data\HouckQuadTransmon'
+settings['meas_type'] = 'Tmeas'
+
+
+settings['Q_Freq'] = 4.20431e9
+settings['Q_Power'] = -11
+settings['CAV_Freq'] = 8.126e9
+settings['CAV_Power'] = -18
+
+Meas_pos = 80e-6
+#Card settings
+settings['segments']         = 1
+settings['reads']            = 1
+settings['averages']         = 40e3
+settings['activeChannels']   = [1,2]
+settings['sampleRate']       = 2e9/8
+settings['trigger_buffer']   = Meas_pos
+settings['meas_window']      = 20e-6
+
+settings['Tau_min'] = 200e-9
+settings['Tau_max'] = 50e-6
+settings['Tau_points'] = 101
+
+settings['Measurement_pos'] = Meas_pos
+
+meas_repeats = 20
+
+T1vec = numpy.zeros(meas_repeats)
+
+for ind in range(meas_repeats):
+    T1 = meas_T1(instruments, settings)
+    T1vec[ind] = T1
+    time.sleep(900)
+    
+plt.plot(T1vec)
