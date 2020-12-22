8570465d8b83438a25ea9eb3d329a9bae812aa50
Martin Ritter <maritter@umd.edu>
2020-12-22 15:11:32 -0700

    Converted to driver based script (get_default_settings and vna_spec methods)

--- Scripts/vna_spec.py
+++ Scripts/vna_spec.py
@@ -12,65 +12,79 @@ import matplotlib.pyplot as plt
 import userfuncs
 import VNAplottingTools as VNAplots
 
-project_dir = r'Z:\Data\HouckQuadTransmon'
-meas_type = 'Spec'
-save_Dir = userfuncs.saveDir(project_dir, meas_type)
-
-name = 'q4specscan'
-stamp = userfuncs.timestamp()
-scanname = name + '_' + stamp
-
-CAV_Attenuation = 30
-Qbit_Attenuation = -10
-
-start_power = -40
-stop_power = 0
-power_points = 21
-
-settings = vna.spec_default_settings()
-settings['channel'] = 1
-settings['avg_time'] = 30
-settings['measurement'] = 'S21'
-settings['start'] = 3.5e9
-settings['stop'] = 4.5e9
-settings['sweep_points'] = 501
-settings['RFpower'] = -25
-settings['RFport'] = 3
-settings['Mport'] = 2
-settings['CAVport'] = 1
-settings['CAVpower'] = -55 + CAV_Attenuation
-settings['CAVfreq'] = 8.12555e9
-settings['ifBW'] = 2e2
-
-powers = np.linspace(start_power, stop_power, power_points)
-
-mags = np.zeros((len(powers), settings['sweep_points']))
-phases = np.zeros((len(powers), settings['sweep_points']))
-
-t0 = time.time()
-for powerind in range(len(powers)):
-    power = powers[powerind]
-    print('Power: {}, final power: {}'.format(power, powers[-1]))
-    settings['RFpower'] = power
-    data = vna.spec_meas(settings)
+def get_default_settings():
+    settings = {}
     
-    vna.autoscale()
+    #Save location
+    settings['scanname']    = 'initial_power_scan_q4'
+    settings['meas_type']   = 'Spec'
+    settings['project_dir'] = r'Z:\Data\HouckQuadTransmon'
+
+    #Sweep parameters
+    settings['CAV_Attenuation'] = 30
+    settings['Qbit_Attenuation'] = 10
+
+    settings['start_power']  = -20
+    settings['stop_power']   = 10
+    settings['power_points'] = 31
+
+    #VNA settings
+    settings['channel'] = 1
+    settings['avg_time'] = 30
+    settings['measurement'] = 'S21'
+    settings['start_freq'] = 3.5e9
+    settings['stop_freq'] = 4.5e9
+    settings['freq_points'] = 501
+    settings['RFpower'] = -25
+    settings['RFport'] = 3
+    settings['Mport'] = 2
+    settings['CAVport'] = 1
+    settings['CAVpower'] = -55
+    settings['CAVfreq'] = 8.12555e9
+    settings['ifBW'] = 2e2
+
+def vna_spec(instruments, settings):
+    #Instruments used
+    vna = instruments['VNA']
+
+    #Data saving and naming
+    saveDir = userfuncs.saveDir(settings['project_dir'], settings['meas_type'])
+    stamp = userfuncs.timestamp()
+    filename = settings['scanname'] + '_' + stamp
+
+    start_power = settings['start_power']
+    stop_power = settings['stop_power']
+    power_points = settings['power_points']
+    powers = np.linspace(start_power, stop_power, power_points)
+
+    mags = np.zeros((len(powers), settings['freq_points']))
+    phases = np.zeros((len(powers), settings['freq_points']))
+
+    t0 = time.time()
+    for powerind in range(len(powers)):
+        power = powers[powerind]
+        print('Power: {}, final power: {}'.format(power, powers[-1]))
+        settings['RFpower'] = power
+
+        data = vna.spec_meas(settings)
+
+        vna.autoscale()
+
+        mags[powerind] = data['mag']
+        phases[powerind] = data['phase']
+
+        if powerind==0:
+            t1=time.time()
+            tdiff = t1-t0
+            ttotal = tdiff*len(powers)
+            print('Single run time: {}, estimated total time: {}'.format(tdiff, ttotal))
+
+    t2 = time.time()
+    print('Elapsed time: {}'.format(t2-t0))
     
-    mags[powerind] = data['mag']
-    phases[powerind] = data['phase']
-    
-    if powerind==0:
-        t1=time.time()
-        tdiff = t1-t0
-        ttotal = tdiff*len(powers)
-        print('Single run time: {}, estimated total time: {}'.format(tdiff, ttotal))
+    freqs = data['xaxis']
 
-t2 = time.time()
-print('Elapsed time: {}'.format(t2-t0))
-  
-freqs = data['xaxis']
+    VNAplots.power_plot(freqs, mags, phases, powers, filename, settings['CAV_Attenuation'])
 
-VNAplots.power_plot(freqs, mags, phases, powers, scanname, CAV_Attenuation)
-
-userfuncs.SaveFull(save_Dir, scanname, ['mags', 'phases', 'freqs', 'powers', 'Qbit_Attenuation', 'CAV_Attenuation'], locals(), expsettings=settings)
-plt.savefig(os.path.join(save_Dir, scanname+'.png'), dpi = 150)
+    userfuncs.SaveFull(saveDir, filename, ['mags', 'phases', 'freqs', 'powers'], locals(), expsettings=settings)
+    plt.savefig(os.path.join(saveDir, filename+'.png'), dpi = 150)
