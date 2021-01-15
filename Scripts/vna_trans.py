1f40c4c01345ac91a35a841a282eafcfd6cb25a7
Martin Ritter <maritter@umd.edu>
2021-01-14 19:27:40 -0500

    Updated to use absolute power going to the fridge on the axes. Also made the powersweep plot update every tick instead of only at the end

--- Scripts/vna_trans.py
+++ Scripts/vna_trans.py
@@ -51,8 +51,8 @@ def vna_trans(instruments, settings):
     CAV_Attenuation = settings['CAV_attenuation']
     scanname = settings['scanname']
 
-    start_power = settings['start_power']
-    stop_power = settings['stop_power']
+    start_power = settings['start_power'] + CAV_Attenuation
+    stop_power = settings['stop_power'] + CAV_Attenuation
     power_points = settings['power_points']
     powers = np.linspace(start_power, stop_power, power_points)
 
@@ -62,7 +62,7 @@ def vna_trans(instruments, settings):
     t0 = time.time()
     for powerind in range(len(powers)):
         power = powers[powerind]
-        print('Power: {}, final power: {}'.format(power, powers[-1]))
+        print('Power: {}, final power: {}'.format(power-CAV_Attenuation, powers[-1]-CAV_Attenuation))
         settings['RFpower'] = power
 
         data = vna.trans_meas(settings)
@@ -77,52 +77,26 @@ def vna_trans(instruments, settings):
             tdiff = t1-t0
             ttotal = tdiff*len(powers)
             print('Single run time: {}, estimated total time: {}'.format(tdiff, ttotal))
+        
+        freqs = data['xaxis']   
 
+        full_data = {}
+        full_data['xaxis'] = freqs
+        full_data['mags'] = mags[0:powerind+1]
+        full_data['phases'] = phases[0:powerind+1]
+    
+        single_data = {}
+        single_data['xaxis'] = freqs
+        single_data['mag'] = data['mag']
+        single_data['phase'] = data['phase']
+    
+        labels = ['Freq (GHz)', 'Power (dBm)']
+        yaxis = powers[0:powerind+1]-CAV_Attenuation
+        plots.simplescan_plot(full_data, single_data, yaxis, scanname, labels, identifier='', fig_num=2)
+    
+        userfuncs.SaveFull(saveDir, filename, ['mags', 'phases', 'freqs', 'powers'], locals(), expsettings=settings)
+        
     t2 = time.time()
     print('Elapsed time: {}'.format(t2-t0))
 
-    freqs = data['xaxis']   
-
-    full_data = {}
-    full_data['xaxis'] = freqs
-    full_data['mags'] = mags
-    full_data['phases'] = phases
-
-    single_data = {}
-    single_data['xaxis'] = freqs
-    single_data['mag'] = mags[-1]
-    single_data['phase'] = phases[-1]
-
-    labels = ['Freq (GHz)', 'Power (dBm)']
-    yaxis = powers
-    plots.simplescan_plot(full_data, single_data, yaxis, scanname, labels, identifier='', fig_num=2)
-    ###Martin's power plot version
-    #VNAplots.power_plot(freqs, mags, phases, powers, scanname, -CAV_Attenuation)
-
-    ####Alicia's general plot version
-    #VNAplots.general_VNAplot(freqs, mags, phases, powers, scanname, 
-    #                                 xlabel = 'Frequency (GHz)', ylabel = 'Power (dB)', identifier = '',
-    #                                 HWattenuation = CAV_Attenuation,
-    #                                 fig_num = 2)
-
-    ####manual plotting example
-    #fig = plt.figure(1,figsize=(13,8))
-    #fig.clf()
-
-    #ax = plt.subplot(1,2,1)
-    #VNAplots.general_colormap_subplot(ax,freqs, powers-CAV_Attenuation, mags)
-    #plt.xlabel('Frequency (GHz)')
-    #plt.ylabel('Power (dB)')
-    #plt.title('S21 mag')
-
-    #ax = plt.subplot(1,2,2)
-    #VNAplots.general_colormap_subplot(ax,freqs, powers-CAV_Attenuation, phases)
-    #plt.xlabel('Frequency (GHz)')
-    #plt.ylabel('Power (dB)')
-    #plt.title('S21 phase')
-
-    #plt.suptitle('Filename: {}'.format(scanname))
-    #plt.show()
-
-    userfuncs.SaveFull(saveDir, filename, ['mags', 'phases', 'freqs', 'powers'], locals(), expsettings=settings)
     plt.savefig(os.path.join(saveDir, filename+'.png'), dpi = 150)
