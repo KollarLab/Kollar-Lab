88584910d54db34b2fe8eb9cd85f95f7cd1665c9
Martin Ritter <maritter@umd.edu>
2021-05-25 19:43:48 -0400

    Updated holzworth driver, baudrate needed to be set to 115200 instead of the default 9600. Still having issues with the instrument though....

--- kollar_instruments/HS9000B.py
+++ kollar_instruments/HS9000B.py
@@ -4,41 +4,60 @@ Created on Thu Dec  3 18:04:20 2020
 
 @author: Kollarlab
 """
+from userfuncs import freeze
+import pyvisa
 
-from .SCPIinst import SCPIinst
-
-class HS9000B(SCPIinst):
-    errcmds           = {}
-    errcmds['error']  = 'SYST:ERR?'
-    errcmds['serror'] = 'SYST:SERR?'
-    
-    commandlist = {}
-    commandlist['core']   = {}
-    commandlist['Mod']     = {}
-    commandlist['Ref']    = {}
-    commandlist['RefOut'] = {}
-    
-    core = {}
-    core['Output']  = 'CH1:PWR:RF'
-    core['Power']  = 'CH1:PWR'
-    core['Freq']   = 'CH1:FREQ'
-    core['Phase'] = 'CH1:PHASE'
-    
-    Mod = {}
-    Mod['Mod']   = 'SOURce:IQ:STATe'
-    Mod['Imp']   = 'SOURce:IQ:IMPairment'
-    
-    Ref = {}
-    Ref['Source']    = 'SOURce:ROSCillator:SOURce'
-    Ref['Frequency'] = 'SOURce:ROSCillator:EXTernal:FREQuency'
-    
-    RefOut = {}
-    RefOut['Source']    = 'CONNector:REFLo:OUTPut'
-    RefOut['Frequency'] = 'SOURce:ROSCillator:OUTPut:FREQuency'
-    
-    commandlist['core']   = core
-    commandlist['Ref']    = Ref
-    commandlist['RefOut'] = RefOut
-
+@freeze
+class HS9000B():
     def __init__(self, address):
-        super().__init__(address, self.commandlist, self.errcmds) 
+        rm = pyvisa.ResourceManager()
+        self.inst = rm.open_resource(address)
+        self.inst.baud_rate = 115200
+
+        self.ch1 = channel(self.inst, 1)
+        self.ch2 = channel(self.inst, 2)
+    
+@freeze
+class channel():
+    def __init__(self, inst, number):
+        self.ID = number
+        self.inst = inst
+
+    @property 
+    def freq(self):
+        return self.inst.query(':CH{}:FREQ?'.format(self.ID))
+    @freq.setter
+    def freq(self, value):
+        self.inst.write(':CH{}:FREQ:{}'.format(self.ID, value))
+    
+    @property
+    def output(self):
+        return self.inst.query(':CH{}:PWR:RF?'.format(self.ID))
+    @output.setter
+    def output(self, state):
+        self.inst.write(':CH{}:PWR:RF:{}'.format(self.ID, value))
+    
+    @property
+    def power(self):
+        return self.inst.query(':CH{}:PWR?'.format(self.ID))
+    @power.setter
+    def power(self, value):
+        self.inst.write(':CH{}:PWR:{}'.format(self.ID, value))
+    
+    @property
+    def phase(self):
+        return self.inst.query(':CH{}:PHASE?'.format(self.ID))
+    @phase.setter
+    def phase(self, value):
+        self.inst.write(':CH{}:PHASe:{}'.format(self.ID, value))
+
+    @property
+    def ext_mod_enable(self):
+        self.inst.query(':CH{}:MOD?'.format(self.ID))
+    
+    @property
+    def ext_mod(self):
+        return self.inst.query(':CH{}:MOD:MODE?'.format(self.ID))
+    @ext_mod.setter
+    def ext_mod(self, mode):
+        self.inst.write(':CH{}:MOD:MODE:{}'.format(self.ID, mode))
