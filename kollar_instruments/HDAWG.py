Fixed a small issue with the output signal amplitude: since we are limited to 3 digits of precision, some combinations of fullscale/ amplitude are better than others. This fix tries to find the "roundest" fraction instead of the smallest fullscale signal size

kollar_instruments/HDAWG.py
@@ -782,8 +782,15 @@ class HDAWGchannel():
             if val > x:
                 continue
             else:
-                self.fullscale = x
-                self.AWGamp    = val/x
+                #Check that the ratio of the full scale to the amplitude is round
+                #to the mV scale. If not, skips this fullscale and tries the next one
+                #This gives us the 'best'/ closest representation on the AWG
+                delta = abs(np.round(val/x,3)-val/x)*1e4
+                if delta>0.001:
+                    print('delta is too large: {}'.format(delta))
+                    continue
+                self.fullscale = np.round(x,3)
+                self.AWGamp    = np.round(val/x,3)
                 break
         self.configured = True
 