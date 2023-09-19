6b6b7880bafc176480982a9c3c2b35f59cd6b7ee
kellenob <106773818+kellenob@users.noreply.github.com>
2023-09-19 17:09:28 -0400

    QICK
    Adding the qick folder to the github

---
+++ qick/__init__.py
@@ -0,0 +1,19 @@
+from .averager_program import AveragerProgram, RAveragerProgram, NDAveragerProgram
+from .qick_asm import QickConfig, QickProgram
+import os
+
+__version__ = "0.2"
+
+def bitfile_path():
+    board2file =  {'ZCU216' :'qick_216.bit',
+            'ZCU111' :'qick_111.bit',
+            'RFSoC4x2' :'qick_4x2.bit'}
+    filename = board2file[os.environ['BOARD']]
+    src = os.path.join(os.path.dirname(qick.__file__), filename)
+    return src
+
+
+try:
+    from .qick import QickSoc
+except Exception as e:
+    print("Could not import QickSoc:", e)
